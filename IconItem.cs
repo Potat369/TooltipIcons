ca7736eab3488cefa2ede42a2020e2edb1628ab7
Potat369 <yevheniidemian@gmail.com>
2024-03-03 13:36:21 +0200

    Fix icon item.

--- IconItem.cs
+++ IconItem.cs
@@ -3,8 +3,15 @@
 namespace TooltipIcons
 {
     [Autoload(false)]
-    internal class IconItem(string name, string texturePath) : ModItem
+    internal class IconItem : ModItem
     {
+        private string name;
+        private string texturePath;
+        public IconItem(string name, string texturePath)
+        {
+            this.name = name;
+            this.texturePath = texturePath;
+        }
         protected override bool CloneNewInstances => true;
         public override string Texture => texturePath;
 
